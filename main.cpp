95f74c8c7b755fc852c4ab9e047ed17e55f4ecd7
Oleg <cetten8@gmail.com>
2021-12-09 13:43:58 +0300

    aaa

--- main.cpp
+++ main.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 
 using namespace std;
-// задача С
+
 int main() {
     int y1, x1, y2, x2;
     cin >> x1 >> y1 >> x2 >> y2;
-    int x3, y3, r, n = 0, now = 1, stx, sty, sy = y1, i = 0, p = 0;
+    long x3, y3, r, n = 0, now = 1, stx, sty, sy = y1;
     cin >> x3 >> y3 >> r;
-    long fin = r * 4 + (r - 1) * (r - 1) * 4;
+    long fin = r * 4 + (r - 1) * (r - 1) * 4 + 1;
     stx = x3 - r;
     sty = y3;
-    long a = abs(x2 - x1), b = abs((y2 - y1)), c = a * b;
-    long long arr[a][c];
-    while (x1 != x2 && y1 != y2){
-        if (y1 < y2){
-            arr[p] = x1;
-            arr[p][i] = y1;
-            y1++;
-            i++;
-        } else {
-            x1++;
-            y1 = sy;
-        }
-    }
     while (now != fin){
-        i = 0;
-        p = 0;
-        if (arr[p] == stx && arr[i] == sty){
-            n++;
+        while (x1 != x2 && y1 != y2){
+            if (x1 == stx && y1 == sty){
+                n++;
+            }
+            if (y1 < y2){
+                y1++;
+            } else {
+                x1++;
+                y1 = sy;
+            }
         }
         if (now == 1){
             stx = stx + 1;
@@ -50,5 +43,6 @@ int main() {
         }
     }
     cout << n;
+
     return 0;
 }
