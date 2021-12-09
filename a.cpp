0980d0c10ed5def4460f3249218374899a28247b
oleg <cetten8@gmail.com>
2021-12-09 13:02:22 +0300

    a

--- a.cpp
+++ a.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-int main() {
+int a() { // (long a, long b) {
     long a, b;
     cin >> a >> b;
-    if ((a + b) % 2 > 0) {
+    if ((a + b) % 2 != 0) {
         // сумма координат не делится на два - не попадём
         cout << "-1";
     }
@@ -54,13 +54,36 @@ int main() {
         else {
             // 3й квадрант - два нужно
             cout << 2 << endl;
-            // первое всегда на 1 1, чтобы ближе
-            cout << "1 1 V" << endl;
             // второе считаем
             long y = (b - a + 2) / 2;
             long x = 2 - y;
             cout << x << " " << y << " H" << endl;
+            // первое всегда на 1 1, чтобы ближе
+            cout << "1 1 V" << endl;
         }
     }
+    return 0;
+}
+
+int amain() {
+    //cout << endl << "Test: 10, 5 = -1" << endl;
+    //a(10, 5);
+    //cout << endl << "Test: 4, 4 = 0" << endl;
+    //a(4, 4);
+    //cout << endl<< "Test: 5, -1 = 2 2 H" << endl;
+    //a(5, -1);
+    //cout << endl << "Test: 0, -2 = 1 1 H, 2 0 V" << endl;
+    //a(0, -2);
+    //cout << endl << "Test: 2, -2 = 1 1 H, 3 -1 V" << endl;
+    //a(2, -2);
+    //cout << endl << "Test: -2 -2 = -1 -3 H | 1 1 H | 2 0 V" << endl;
+    //a(-2, -2);
+    //cout << endl << "Test: -2 4 = 1 1 V" << endl;
+    //a(-2, 4);
+    //cout << endl << "Test: -2 0 = 1 1 V | 0 2 H" << endl;
+    //a(-2, 0);
+    //cout << endl << "Test: -2 2 = 1 1 V | -1 3 H" << endl;
+    //a(-2, 2);
+
     return 0;
 }
